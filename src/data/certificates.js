1d8c1eac6336c57bf394d1d74a6a04816903976d
AMIRULHAFIZI <128666192+AMIRULHAFIZI@users.noreply.github.com>
2026-05-21 08:59:22 +0800

    Fix Certificate Date

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -4,21 +4,21 @@ const certificates = [
     title: "iPetro Workshop Certificate",
     issuer: "iPetro",
     file: "/certs/iPetro_Certificate.pdf",
-    date: "2024"
+    date: "2025"
   },
   {
     id: "google-cloud",
     title: "Google Cloud Certificate",
     issuer: "Google",
     file: "/certs/Google_Cloud_Certificate.pdf",
-    date: "2025"
+    date: "2024"
   },
   {
     id: "comptia-data-plus",
     title: "CompTIA Data+ Certificate",
     issuer: "CompTIA",
     file: "/certs/CompTIA_DataPlus_Certificate.pdf",
-    date: "2026"
+    date: "2025"
   }
 ];
 
